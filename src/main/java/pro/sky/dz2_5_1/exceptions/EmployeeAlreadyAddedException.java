579e1d072e9eed7e7325335400057a23221130e8
NickLaye <nicklaye@mail.ru>
2022-06-12 22:02:56 +0300

    Готовое ДЗ

---
+++ src/main/java/pro/sky/dz2_5_1/exceptions/EmployeeAlreadyAddedException.java
@@ -0,0 +1,11 @@
+package pro.sky.dz2_5_1.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.BAD_REQUEST)
+public class EmployeeAlreadyAddedException extends RuntimeException{
+
+
+
+}
